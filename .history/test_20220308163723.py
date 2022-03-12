ac1a0e5e0de2a847748d8d8229789828be6cda2d
fxg <2283494609@qq.com>
2022-03-12 17:44:24 +0800

    0312

---
+++ .history/test_20220308163723.py
@@ -0,0 +1,15 @@
+from flask import Flask, flash, redirect, render_template, \
+    request, url_for
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/index')
+@app.route('/')
+def index():
+    return 'hello world'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
